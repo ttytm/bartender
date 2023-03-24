e9bf9f90c79d5fbb0ebb66e13615837f8afcc6d9
Turiiya <34311583+tobealive@users.noreply.github.com>
2023-03-24 14:57:51 +0100

    followup(8b37635): don't forget renamed smooth example

---
+++ examples/smooth.v
@@ -0,0 +1,38 @@
+module main
+
+import bartender { SmoothBar, Theme, ThemeVariant }
+
+fn main() {
+	mut b := SmoothBar{
+		label: ['Push Fill', 'Done!']!
+		theme: Theme.push
+	}
+	mut b2 := SmoothBar{
+		label: ['Pull Fill', 'Finished!']!
+		theme: Theme.pull
+	}
+
+	for _ in 0 .. b.width {
+		b.progress()
+	}
+	for _ in 0 .. b2.width {
+		b2.progress()
+	}
+
+	// Re-use bars
+	b = SmoothBar{
+		label: ['Push Drain', 'Completed!']!
+		theme: ThemeVariant{.push, .drain}
+		border: ['', '']!
+	}
+	b.prep()
+	for _ in 0 .. b.width {
+		b.progress()
+	}
+	b2.label = ['Pull Drain', 'Ready!']!
+	b2.theme = ThemeVariant{.pull, .drain}
+	b2.prep()
+	for _ in 0 .. b2.width {
+		b2.progress()
+	}
+}
