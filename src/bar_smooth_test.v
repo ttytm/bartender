a3949a2cde0af88533bf9ef69a86823d20d9bc42
Turiiya <34311583+tobealive@users.noreply.github.com>
2023-04-26 18:30:26 +0200

    test: finished smooth bar

--- src/bar_smooth_test.v
+++ src/bar_smooth_test.v
@@ -1,9 +1,12 @@
 module bartender
 
-const test_bar = SmoothBar{
-	width: 40
-	post: ' Loading...'
-}
+const (
+	test_bar = SmoothBar{
+		width: 40
+		post: Affix{' Loading...', ' Finished!'}
+	}
+	col_iters = [' ', '▏', '▎', '▍', '▌', '▋', '▊', '▉', '█'].len
+)
 
 fn test_setup() {
 	mut b := bartender.test_bar
@@ -11,7 +14,7 @@ fn test_setup() {
 	assert b.theme == ThemeChoice(Theme.push)
 	assert b.state.pos == 0
 	assert b.width_ == b.width
-	assert b.iters == [' ', '▏', '▎', '▍', '▌', '▋', '▊', '▉', '█'].len * b.width
+	assert b.iters == bartender.col_iters * b.width
 }
 
 fn test_progress() {
@@ -33,9 +36,13 @@ fn test_progress() {
 	}
 	assert b.format() == '█▌                                       Loading...'
 	// 20 5/9
-	for i := 0; i < 9 * 19; i++ {
+	for i := 0; i < bartender.col_iters * 19; i++ {
 		b.progress()
 	}
 	assert b.format() == '████████████████████▌                    Loading...'
-	// TODO: test finished
+
+	for i := 0; i < bartender.col_iters * 19 + 4; i++ {
+		b.progress()
+	}
+	assert b.format() == '████████████████████████████████████████ Finished!'
 }
