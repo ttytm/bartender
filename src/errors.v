5c1b4572dda60160380552d5d0814d388ece1103
Turiiya <34311583+tobealive@users.noreply.github.com>
2023-06-13 19:12:32 +0200

    feat!: return errors as values where appropriate
    This change only concerns the `BarType.progress()` methods and is a
    breaking change.

--- src/errors.v
+++ src/errors.v
@@ -3,19 +3,26 @@ module bartender
 struct BarError {
 	Error
 	kind ErrorKind
-	msg  string
+	val  string
 }
 
 enum ErrorKind {
-	finished
+	already_finished
 	delay_exceeded
 	missing_multi
 }
 
 fn (err BarError) msg() string {
 	return match err.kind {
-		.finished { 'Trying to progress already finished bar.' }
+		.already_finished { 'Trying to progress already finished bar.' }
 		.delay_exceeded { 'Delay cannot exceed 100%.' }
-		.missing_multi { 'Failed drawing bars. []&Bar indices not set as multi: ${err.msg}' }
+		.missing_multi { 'Failed drawing bars. []&Bar indices not set as multi: ${err.val}' }
 	}
+}
+
+fn bar_error(kind ErrorKind, val ?string) IError {
+	return IError(BarError{
+		kind: kind
+		val: val or { '' }
+	})
 }
